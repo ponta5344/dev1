66cbc23753f7c147a72c8b686971268a39a403ed
ponta_win <ponta5344@zoo.email.ne.jp>
2020-10-25 17:02:54 +0900

    不要プロジェクト削除

--- TEST2/src/ponta/test/TestPonta2.java
+++
@@ -1,9 +0,0 @@
-package ponta.test;
-
-public class TestPonta2 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello! Ponta2");
-	}
-
-}
--- TEST3/src/ponta/test/TestPonta3.java
+++
@@ -1,9 +0,0 @@
-package ponta.test;
-
-public class TestPonta3 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello! Ponta3");
-	}
-
-}
